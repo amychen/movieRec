03fec5696c3ba6d4e1a87f0bc266d3f27ac74bb6
amychen <ac6325@nyu.edu>
2019-12-06 17:43:45 +0800

    converted zipcode into states

--- convertToCSV.py
+++ convertToCSV.py
@@ -1,4 +1,5 @@
 import csv
+import zipcodes
 
 def convertToCSV(filename):
     with open('ml-1m/' + filename + ".dat") as dat_file, open('ml-1m/' + filename + '.csv', 'w') as csv_file:
@@ -8,14 +9,25 @@ def convertToCSV(filename):
             row = [field.strip() for field in line.split('::')]
             csv_writer.writerow(row)
 
+def convertZipCodeToState(filename):
+    with open('ml-1m/' + filename) as csv_file, open('ml-1m/' + 'user_zip.csv', 'w') as csv_zip_file:
+        csv_reader = csv.reader(csv_file)
+        csv_writer = csv.writer(csv_zip_file)
+
+        for line in csv_reader:
+            zipcode = line[-1]
+            try:
+                zipcode = zipcodes.matching(str(zipcode))[0]['state']
+                line[-1] = zipcode
+            except:
+                continue
+            csv_writer.writerow(line) 
+
 def main():
-   convertToCSV('movies'); 
-   convertToCSV('ratings'); 
-   convertToCSV('users'); 
+   convertToCSV('movies') 
+   convertToCSV('ratings') 
+   convertToCSV('users')
+   convertZipCodeToState('users.csv')
 
 if __name__ == "__main__":
     main()
-
-
-
-
